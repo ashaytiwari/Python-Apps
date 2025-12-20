a321628cd0e2c2577b6851f59ad70fd953b64ac7
ashaytiwari <ashaytiwari2001@gmail.com>
2025-12-21 01:43:45 +0530

    added edit task feature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 tasks = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
 
     match user_action.strip():
         
@@ -10,8 +10,17 @@ while True:
             tasks.append(todo)
 
         case "show":
+            number = 1
             for item in tasks:
-                print(item)
+                print(f"{number}. {item.title()}")
+                number = number + 1
+
+        case "edit":
+            stringifiedNumber = input("Number of the task to edit: ")
+            number = int(stringifiedNumber)
+            newTask = input("Enter new task: ")
+            tasks[number - 1] = newTask
+            print("Task Updated!")
 
         case "exit":
             break
