a956c644fa2ec1b8a0127971e7fb5648d3a03053
ashaytiwari <ashaytiwari2001@gmail.com>
2025-12-21 00:36:23 +0530

    added basic logic for command line instructions to manage tasks

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Enter todo:")
+tasks = []
+
+while True:
+    user_action = input("Type add, show or exit: ")
+
+    match user_action:
+        
+        case "add":
+            todo = input("Enter Task: ")
+            tasks.append(todo)
+
+        case "show":
+            print(tasks)
+
+        case "exit":
+            break
