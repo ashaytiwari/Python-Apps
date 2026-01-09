975cb20fb79155d0244a86ed2fdeadf3ff1b6eb5
ashaytiwari <ashaytiwari2001@gmail.com>
2026-01-09 23:48:16 +0530

    feat: handled tasks_lists and edit_action handlers

--- gui_app.py
+++ gui_app.py
@@ -12,6 +12,12 @@ def parse_tasks_list(_tasks):
     return proper_tasks_list
 
 
+def get_actual_task_text(_task):
+    
+    task_text = _task.split('.')[1].strip()
+    return task_text
+
+
 title = sg.Text("Task Manager App: Manage your Tasks easily here!", font=('Helvetica', 18))
 
 label = sg.Text("Type in a Task:")
@@ -21,18 +27,21 @@ add_button = sg.Button("Add", key='add_action')
 tasks = utilities.get_tasks("tasks.txt")
 tasks_list = parse_tasks_list(tasks)
 
+edit_info_label = sg.Text("Click from the below list to edit the task.")
+
 task_list_box = sg.Listbox(
     values=tasks_list, 
     key="task_lists", 
     enable_events=True,
     size=[45, 10]
     )
+edit_button = sg.Button("Edit", key='edit_action')
 
-layout = [[title], [label], [task_input, add_button], [task_list_box]]
+
+layout = [[title], [label], [task_input, add_button], [edit_info_label], [task_list_box, edit_button]]
 font = ('Helvetica', 14)
 
 window = sg.Window("Task Manager App", layout, font=font)
-
 
 while True:
     event, values = window.read()
@@ -51,6 +60,38 @@ while True:
             """ update UI """
             window["task"].update(value='')
             window["task_lists"].update(values=tasks_list)
+
+        case "task_lists":
+            
+            """
+              Extract the exact tasks:
+              1. Get the values['task_lists'][0] that would be "1. Buy Books"
+              2. Split the above value that would be [1, ' Buy Books']
+              3. Get the second value from thr above array and then strip that string
+            """
+            tasks_to_edit = get_actual_task_text(values['task_lists'][0])
+            window["task"].update(value=tasks_to_edit)
+
+        case "edit_action":
+            
+            try:
+                selected_task = get_actual_task_text(values['task_lists'][0]) + '\n'
+                edited_task = values['task']
+
+                all_tasks = utilities.get_tasks('tasks.txt')
+                editing_index = all_tasks.index(selected_task)
+
+                all_tasks[editing_index] = edited_task + '\n'
+                utilities.write_tasks(all_tasks)
+
+                parsed_tasks_list = parse_tasks_list(all_tasks)
+
+                """ update ui """
+                window["task"].update(value='')
+                window["task_lists"].update(values=parsed_tasks_list)
+
+            except:
+                print('Something went wrong, check console!')
 
         case sg.WIN_CLOSED:
             break
