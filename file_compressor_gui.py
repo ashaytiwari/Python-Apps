d8952dc7690ef97ef8ce22d12fcee13b4aae1781
ashaytiwari <ashaytiwari2001@gmail.com>
2026-01-10 00:36:06 +0530

    feat: created file compressor gui application

--- file_compressor_gui.py
+++ file_compressor_gui.py
@@ -1,25 +1,47 @@
 import FreeSimpleGUI as sg
+import utilities
 
 label = sg.Text('Compress your files in one go!')
 
 label1 = sg.Text('Select files to compress:')
-input1 = sg.Input()
-button1 = sg.FilesBrowse('Choose')
+input1 = sg.Input(key="selected_files")
+button1 = sg.FilesBrowse('Choose', key="choose_files")
 
 label2 = sg.Text('Select destination folder:')
-input2 = sg.Input()
-button2 = sg.FolderBrowse('Choose')
+input2 = sg.Input(key="selected_path")
+button2 = sg.FolderBrowse('Choose', key="choose_path")
 
 row1 = [label1, input1, button1]
 row2 = [label2, input2, button2]
 
-compressButton = sg.Button('Compress')
+compressButton = sg.Button('Compress', key="compress")
+response_text = sg.Text(key='response')
 
-layout = [[label], [row1], [row2], [compressButton]]
+layout = [[label], [row1], [row2], [compressButton, response_text]]
 
 window = sg.Window('Compress Files', layout)
 
-# Display and interact with the Window
-event, values = window.read()   
+while True:
+    event, values = window.read()
+    print(event, values)
+
+    if values['selected_files'] == '' or values['selected_path'] == '':
+        window['response'].update(value="Please select files and destination folder before compressing!", text_color='pink', )
+        continue
+
+    match event:
+        
+        case "compress":
+            filepaths = values['selected_files'].split(';')
+            folder_path = values['selected_path']
+
+            utilities.make_zip_archive(filepaths, folder_path)
+
+            window['response'].update(value="File Compressed successfully!", text_color='green')
+            window['selected_files'].update(value='')
+            window['selected_path'].update(value='')
+
+        case sg.WIN_CLOSED:
+            break;   
 
 window.close()
--- utilities.py
+++ utilities.py
@@ -1,3 +1,7 @@
+import zipfile
+import pathlib
+import time
+
 FILEPATH = "tasks.txt"
 
 """ Read tasks from the text file. """
@@ -14,3 +18,14 @@ def write_tasks(data, filepath=FILEPATH):
     
     with open(filepath, 'w') as file_local:
         file_local.writelines(data)
+
+
+""" Create Zip Archive """
+def make_zip_archive(filepaths, destination_folder):
+    
+    destination_path = pathlib.Path(destination_folder, f"compressed-{time.time()}.zip")
+
+    with zipfile.ZipFile(destination_path, 'w') as archive:
+        for filepath in filepaths:
+            filepath_local = pathlib.Path(filepath)
+            archive.write(filepath, arcname=filepath_local.name)
