483a6170f6ad428d123031b5b421b1683fd564eb
ashaytiwari <ashaytiwari2001@gmail.com>
2026-01-22 18:41:28 +0530

    feat: added lined pages for pdf

--- pdf_generator.py
+++ pdf_generator.py
@@ -2,11 +2,17 @@ from fpdf import FPDF
 import pandas as pd
 
 def set_footer(topic, ln):
-    pdf.ln(ln)
+    # pdf.ln(ln)
     
     pdf.set_font(family='Arial', style='I', size=10)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=5, text=topic, ln=1, align='R')
+
+def set_lines(number_of_lines):
+    
+    for i in range(number_of_lines):
+        pdf.ln(13)
+        pdf.cell(w=0, h=0, text="", ln=1, align='L', border='B')
 
 
 pdf = FPDF(orientation='P', unit="mm", format="A4")
@@ -21,11 +27,13 @@ for index, row in dataframe.iterrows():
     pdf.cell(w=0, h=5, text=f"{row["Order"]}. {row["Topic"]}", ln=1, align='L')
     pdf.line(x1=10, y1=17, x2=200, y2=17)
 
+    set_lines(20)
     set_footer(topic=row["Topic"], ln=270)
 
     """subtracting 1 page from the mentioned pages as one page is already added above"""
     for i in range(row['Pages'] - 1):
         pdf.add_page(same=True)
+        set_lines(20)
         set_footer(topic=row["Topic"], ln=273)
 
 pdf.output('output.pdf')
