2c8a3dddbedef3ae9ad403084f82ccdc13e720eb
ashaytiwari <ashaytiwari2001@gmail.com>
2026-01-22 17:46:16 +0530

    feat: read data from csv file as dataframe using panda library, implemented csv based pdf generation

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,13 +1,15 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation='P', unit="mm", format="A4")
 
-pdf.add_page()
+dataframe = pd.read_csv("files/pdf-topics.csv")
 
-pdf.set_font(family='Arial', style='B', size=16)
-pdf.cell(w=0, h=5, txt="Introduction", ln=1, align='L')
-
-pdf.set_font(family='Arial', size=10)
-pdf.cell(w=0, h=10, txt="Work Stream", ln=1, align='L')
+for index, row in dataframe.iterrows():
+    pdf.add_page()
+    pdf.set_font(family='Arial', style='B', size=16)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=5, txt=f"{row["Order"]}. {row["Topic"]}", ln=1, align='L')
+    pdf.line(10, 17, 200, 17)
 
 pdf.output('output.pdf')
