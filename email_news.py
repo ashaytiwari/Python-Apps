856b9d71c32b2c2ab539412b07330ddf088e0880
ashaytiwari <ashaytiwari2001@gmail.com>
2026-01-25 00:42:36 +0530

    feat: integrated news api and created send email utility

--- email_news.py
+++ email_news.py
@@ -1,17 +1,30 @@
+import os
 import requests
 from dotenv import load_dotenv
-import os
+import smtplib, ssl
 
 load_dotenv()
 
-NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+NEWS_API_URL = os.getenv("NEWS_API_URL")
+SMTP_ACCOUNT_EMAIL_ADDRESS = os.getenv("SMTP_ACCOUNT_EMAIL_ADDRESS")
+SMTP_ACCOUNT_EMAIL_PASSWORD = os.getenv("SMTP_ACCOUNT_EMAIL_PASSWORD")
+SMTP_RECEIVER_EMAIL_ADDRESS = os.getenv("SMTP_RECEIVER_EMAIL_ADDRESS")
 
-print(NEWS_API_KEY)
 
-# url = "https://jsonplaceholder.typicode.com/posts"
+"""fn to send email to the hardcoded user using hardcoded account"""
+def send_email(message):
+    
+    host = "smtp.gmail.com"
+    port = 465
 
-# request = requests.get(url)
-# content = request.text
+    context = ssl.create_default_context()
 
-# print(request)
-# print(content)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(SMTP_ACCOUNT_EMAIL_ADDRESS, SMTP_ACCOUNT_EMAIL_PASSWORD)
+        server.sendmail(SMTP_ACCOUNT_EMAIL_ADDRESS, SMTP_RECEIVER_EMAIL_ADDRESS, message)
+
+
+response = requests.get(NEWS_API_URL)
+content = response.json()
+
+print(content['articles'][0])
