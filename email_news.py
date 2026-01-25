d496094619b01fd303ba8131fb246c5c1db61607
ashaytiwari <ashaytiwari2001@gmail.com>
2026-01-26 00:43:11 +0530

    feat: updated email templates to send email from the news

--- email_news.py
+++ email_news.py
@@ -2,6 +2,8 @@ import os
 import requests
 from dotenv import load_dotenv
 import smtplib, ssl
+from email.mime.text import MIMEText
+import sys
 
 load_dotenv()
 
@@ -24,7 +26,43 @@ def send_email(message):
         server.sendmail(SMTP_ACCOUNT_EMAIL_ADDRESS, SMTP_RECEIVER_EMAIL_ADDRESS, message)
 
 
+"""fn to generate html template based on articles details"""
+def generate_html_template(articles):
+    
+    if len(articles) == 0:
+        return None
+    
+    main_template = ''
+
+    for index, item in enumerate(articles[:10]):
+        main_template = main_template + f"<h2>{index + 1}. {item['title']}</h2> \
+          <p>{item['description']}</p> \
+          <a href={item['url']}>{item['url']}</a>  <br /><br />"
+    
+    html_template = f"""\
+      <html>
+        <body>
+          {main_template}
+        </body>
+      </html>
+    """
+
+    return html_template
+
+
+subject = "Today's News"
+
 response = requests.get(NEWS_API_URL)
 content = response.json()
 
-print(content['articles'][0])
+html_body = generate_html_template(content['articles'])
+
+if html_body == None:
+    print("No news found")
+    sys.exit(1)
+
+# Create the email
+message = MIMEText(html_body, "html")
+message["Subject"] = subject
+
+send_email(message.as_string())
